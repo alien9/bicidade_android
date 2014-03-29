d7ee5523939f1eff00452d157291ec5a2e145f13
Tiago Barufi <tiago@homembala.com.br>
2014-03-29 13:14:46 -0300

    bicidade android caminhando\!

---
+++ src/net/bemok/bicidade/DatabaseHandler.java
@@ -0,0 +1,35 @@
+package net.bemok.bicidade;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+	// All Static variables
+	// Database Version
+	private static final int DATABASE_VERSION = 1;
+
+	// Database Name
+	private static final String DATABASE_NAME = "bicidade";
+
+	public DatabaseHandler(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	// Creating Tables
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE position (id INTEGER PRIMARY KEY, zoom NUMERIC, x NUMERIC, y NUMERIC);");
+	}
+
+	// Upgrading database
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Drop older table if existed
+		db.execSQL("DROP TABLE IF EXISTS position");
+
+		// Create tables again
+		onCreate(db);
+	}
+}
